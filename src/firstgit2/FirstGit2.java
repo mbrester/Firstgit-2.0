60cd8a904c0e0256c04dc99c9a0fe4812899a659
mbrester1 <mbrester1@L11709042772.wctc.msft>
2015-01-22 17:05:18 -0600

    First commit, adding project to git

--- src/firstgit2/FirstGit2.java
+++ src/firstgit2/FirstGit2.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package firstgit2.pkg0;
+package firstgit2;
 
 /**
  *
  * @author mbrester1
  */
-public class FirstGit20 {
+public class FirstGit2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+        
+        
     }
     
 }
